25ac1d26cdcb2dd8f95d28cdb5caca0bca05a903
fundamental <mark.d.mccurry@gmail.com>
2017-08-16 17:02:35 -0400

    Update Test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,9 @@
 using Curvelet
 using Base.Test
 
-@test 1 == 1
+N           = 512
+sample_data = randn(N,N)
+plan        = Curvelet.planCurveletTransform(N, Curvelet.C1)
+tmp         = Curvelet.curveletTransform(sample_data, plan)
+sample_hat  = Curvelet.inverseCurveletTransform(tmp, plan)
+@test norm(sample_hat.-sample_hat) < 0.01
